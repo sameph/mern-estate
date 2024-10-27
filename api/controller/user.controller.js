f1822b6d6c2864a2a6fef37b78f40ae943ea363a
sameph <samuelephrem2012@gmail.com>
2024-10-28 01:14:16 +0300

    Add contact landlord functionality

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -62,3 +62,15 @@ export const getUserListing = async (req, res, next) => {
   }
 
 }
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id)
+
+    if (!user) return next(errorHandler(404, "User not found!"));
+    const { password: pass,...rest } = user._doc;
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  } 
+}
