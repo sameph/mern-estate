dacce02ef649c77fb44c3e1c2dfb1d8085a74701
sameph <samuelephrem2012@gmail.com>
2024-10-27 04:14:16 +0300

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createListings, deleteListing } from '../controller/listing.controller.js';
+import { createListings, deleteListing, updateListing } from '../controller/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListings);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
