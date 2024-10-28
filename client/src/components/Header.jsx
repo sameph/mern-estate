6366b25acdc4b636fbbe8164d7c72d0de1fdac25
sameph <samuelephrem2012@gmail.com>
2024-10-28 16:16:06 +0300

    fix error on search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
   const handleSubmit = (e) => {
     e.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set("search", searchTerm);
+    urlParams.set("searchTerm", searchTerm);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
