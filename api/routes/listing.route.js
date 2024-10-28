5df0eff7c31400d2ddc2e00ce79fb2cf77ba1ecf
sameph <samuelephrem2012@gmail.com>
2024-10-28 15:26:12 +0300

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListings, deleteListing, updateListing, getListing } from '../controller/listing.controller.js';
+import { createListings, deleteListing, updateListing, getListing, getListings } from '../controller/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/create', verifyToken, createListings);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get/', getListings);
 
 export default router;
