2789105db88e646d8f598c8e9529dac04bd691db
sameph <samuelephrem2012@gmail.com>
2024-10-29 13:51:28 +0300

    fix signin capitalization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
 import Home from './pages/Home'
-import Signin from './pages/SignIn'
+import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
