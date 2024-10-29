b0c4626cb93018c253592b86a2756920efb79762
sameph <samuelephrem2012@gmail.com>
2024-10-29 13:50:16 +0300

    bild and signin fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
       <Route path="/search" element={<Search />} />
